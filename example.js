e17309580e94a6d471c3f412973ae4f1d8900827
SchizoDuckie <schizoduckie@gmail.com>
2015-12-10 21:27:02 +0100

    some docs on example.js, direct pipe on formatNeedsTranscode == false

--- example.js
+++ example.js
@@ -6,46 +6,77 @@ var net = require('net');
 var port = 8989
 var expressServer;
 
-expressServer = app.listen(port, function () {
-  var host = expressServer.address().address;
-  var port = expressServer.address().port;
+/**
+ * This example starts an express server and serves one url: "/request-from-chromecast"
+ * Pass this url to your chromecast to load on the endpoint provided at startup
+ * to have the video automagically transcoded through FFMPEG
+ */
 
-  console.log('FFmpeg Webserver listening at http://%s:%s', host, port);
+expressServer = app.listen(port, function() {
+    var host = expressServer.address().address;
+    var port = expressServer.address().port;
+
+    console.log('FFmpeg Webserver listening at http://%s:%s', host, port);
 });
 
-app.get("/request-from-chromecast", function (req, res) {
+app.get("/request-from-chromecast", function(req, res) {
     var file = "Zero.Tolerance.2015.HDRip.XviD.AC3-EVO.avi"
     var stats = fs.statSync(file);
     var engine = encoder.profile(encoder.profiles.CHROMECAST, stats.size);
     console.log("Engine::", engine);
     // Sometimes FFmpeg may need to seek throughout a file to encode the video, or probe.
-    engine.on("streamNeeded", function (startByte, endByte, cb) {
+    engine.on("streamNeeded", function(startByte, endByte, cb) {
         // <Error?> Stream.
         console.info("streamNeeded", startByte, endByte);
-        cb(fs.createReadStream(file, {start: startByte, end: endByte}));
+        cb(fs.createReadStream(file, {
+            start: startByte,
+            end: endByte
+        }));
     });
 
     // Certain devices need specific HTTP Headers in order to decode the video. Use these headers. (DLNA ETC...)
-    engine.once("httpHeaders", function (headerInfo) {
+    engine.once("httpHeaders", function(headerInfo) {
         res.writeHeader(headerInfo);
     });
 
-    engine.canPlay(function (canPlay) {
-      console.log("engine.canPlay", canPlay);
-      if (canPlay) {
-        // just send file. DO IT YOUR OWN WAY BITCH
-      }
-      else {
-        encoder.encode(engine, {
-          startTime: "00:00"
-        }, function (stream) {
-          stream.pipe(res, {end: true});
-          stream.on("end", function () {
-            console.log("stream ended");
-            res.end();
-          });
-        });
-      }
+    /**
+     * analyze the file stream to see what type of media we're dealing with and if it's playable
+     *
+     * the mediaInfo object tells you all you need: {
+     *      isAudioMedia: boolean,
+     *      isVideoMedia: boolean,
+     *      audioNeedsTranscoding: boolean
+     *      videoNeedsTranscoding: boolean
+     *      formatNeedsTranscoding: boolean,
+     * }
+     */
+    engine.analyze(function(mediaInfo) {
+
+        console.log("mediaInfo", mediaInfo);
+        // you can skip the unneeded transcode if you care about performance or
+        // flexibility. 
+        if (mediaInfo.formatNeedsTranscoding) {
+            encoder.encode(engine, {
+                startTime: "00:00"
+            }, function(stream) {
+                stream.pipe(res, {
+                    end: true
+                });
+                stream.on("end", function() {
+                    console.log("stream ended");
+                    res.end();
+                });
+            });
+        } else {
+            // would this do? it wouldnt right? range request n shit?
+            stream.pipe(res, {
+                end: true
+            });
+            stream.on("end", function() {
+                console.log("stream ended");
+                res.end();
+            });
+        }
     });
 
-}); 
+});
