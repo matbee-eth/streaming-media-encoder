54e25681da020386f14a462276436d68646a9697
mat <mat@TPMs-Mac-Pro-206.local>
2015-12-09 22:02:29 -0500

    More succinct example index.js

--- index.js
+++ index.js
@@ -17,45 +17,43 @@ expressServer = app.listen(port, function () {
   ready();
 });
 
-var ready = function () {
-  app.get("/request-from-chromecast", function (req, res) {
-      var file = "Zero.Tolerance.2015.HDRip.XviD.AC3-EVO.avi"
-      var stats = fs.statSync(file);
-      var engine = encoder.profile(encoder.profiles.CHROMECAST, stats.size);
-      console.log("Engine::", engine);
-      // Sometimes FFmpeg may need to seek throughout a file to encode the video, or probe.
-      engine.on("streamNeeded", function (startByte, endByte, cb) {
-          // <Error?> Stream.
-          console.info("streamNeeded", startByte, endByte);
-          cb(fs.createReadStream(file, {start: startByte, end: endByte}));
-      });
+app.get("/request-from-chromecast", function (req, res) {
+    var file = "Zero.Tolerance.2015.HDRip.XviD.AC3-EVO.avi"
+    var stats = fs.statSync(file);
+    var engine = encoder.profile(encoder.profiles.CHROMECAST, stats.size);
+    console.log("Engine::", engine);
+    // Sometimes FFmpeg may need to seek throughout a file to encode the video, or probe.
+    engine.on("streamNeeded", function (startByte, endByte, cb) {
+        // <Error?> Stream.
+        console.info("streamNeeded", startByte, endByte);
+        cb(fs.createReadStream(file, {start: startByte, end: endByte}));
+    });
 
-      // Certain devices need specific HTTP Headers in order to decode the video. Use these headers. (DLNA ETC...)
-      engine.once("httpHeaders", function (headerInfo) {
-          res.writeHeader(headerInfo);
-      });
+    // Certain devices need specific HTTP Headers in order to decode the video. Use these headers. (DLNA ETC...)
+    engine.once("httpHeaders", function (headerInfo) {
+        res.writeHeader(headerInfo);
+    });
 
-      // encoder.probe is currently needed before canPlay, will figure it out.
-      encoder.probe(engine, {}, function (err, metadata) {
-        console.log("Probe", err, metadata);
-        engine.canPlay(function (canPlay) {
-          console.log("engine.canPlay", canPlay);
-          if (canPlay) {
-            // just send file. DO IT YOUR OWN WAY BITCH
-          }
-          else {
-            encoder.encode(engine, {
-              startTime: "00:00"
-            }, function (stream) {
-              stream.pipe(res, {end: true});
-              stream.on("end", function () {
-                console.log("stream ended");
-                res.end();
-              });
+    // encoder.probe is currently needed before canPlay, will figure it out.
+    encoder.probe(engine, {}, function (err, metadata) {
+      console.log("Probe", err, metadata);
+      engine.canPlay(function (canPlay) {
+        console.log("engine.canPlay", canPlay);
+        if (canPlay) {
+          // just send file. DO IT YOUR OWN WAY BITCH
+        }
+        else {
+          encoder.encode(engine, {
+            startTime: "00:00"
+          }, function (stream) {
+            stream.pipe(res, {end: true});
+            stream.on("end", function () {
+              console.log("stream ended");
+              res.end();
             });
-          }
-        });
+          });
+        }
       });
+    });
 
-  }); 
-}
+}); 
