aa972030e55e4d85cd86b47293012756e65874a6
SchizoDuckie <schizoduckie@gmail.com>
2015-12-24 23:01:36 +0100

    debug gui w.i.p.

--- DeviceList.js
+++ DeviceList.js
@@ -1,95 +1,101 @@
 var util = require('util'),
-	EventEmitter = require('events'),
-	// dlna
- 	RendererFinder = require('renderer-finder'),
-	MediaRendererClient = require('upnp-mediarenderer-client'),
-	DMRDevice = require('./devices/DigitalMediaRenderer'),
-	// chromecast
-	chromecasts = require('chromecasts'),
-	ChromeCastDevice = require('./devices/ChromeCast');
-	// appletv
+    EventEmitter = require('events'),
+    // dlna
+    RendererFinder = require('renderer-finder'),
+    MediaRendererClient = require('upnp-mediarenderer-client'),
+    DMRDevice = require('./devices/DigitalMediaRenderer'),
+    // chromecast
+    chromecasts = require('chromecasts'),
+    ChromeCastDevice = require('./devices/ChromeCast');
+// appletv
 
 function DeviceList() {
-	EventEmitter.call(this);
-	var self = this;
+    EventEmitter.call(this);
+    var self = this;
 
-	this.devices = {
-		CHROMECAST : {},
-		DLNA : {},
-		APPLETV: {}
-	};
+    this.devices = {
+        CHROMECAST: {},
+        DLNA: {},
+        APPLETV: {}
+    };
 
-	this.chromecastSearching = false;
-	this.dlnaSearching = false;
-	this.appleTVSearching = false;
+    this.chromecastSearching = false;
+    this.dlnaSearching = false;
+    this.appleTVSearching = false;
 
-	this.discover = function() {
-		console.log("Starting discovery");
-		this.findChromeCasts();
-		this.findDMRs();
-		this.emit("devicelist:discovering", this);
-		return new Promise(function(resolve) {
-			setTimeout(function() {
-				resolve(self.devices);
-			}, 500);
-		});
-	};
+    this.discover = function() {
+        console.log("Starting discovery");
+        this.findChromeCasts();
+        this.findDMRs();
+        this.emit("devicelist:discovering", this);
+        return new Promise(function(resolve) {
+            setTimeout(function() {
+                resolve(self.devices);
+            }, 1000);
+        });
+    };
 
-	this.findChromeCasts = function() {
-		if(!this.chromecastSearching) {
-			console.log("Searching for chromecast devices");
-			this.chromecastSearching = true;
-			chromecasts().on('update', function (player) {
-				console.log("Found a chromecast", player.name);
-				player = new ChromeCastDevice(player);
-		    	if(!(player.id in self.devices.CHROMECAST)) {
-		    		self.devices.CHROMECAST[player.id] = player;
-					self.emit("devicelist:newdevice", {type: "ChromeCast", device: player});
-		    	}
-			});
-		}
-	};
+    this.findChromeCasts = function() {
+        if (!this.chromecastSearching) {
+            console.log("Searching for chromecast devices");
+            this.chromecastSearching = true;
+            chromecasts().on('update', function(player) {
+                console.log("Found a chromecast", player.name);
+                player = new ChromeCastDevice(player);
+                if (!(player.id in self.devices.CHROMECAST)) {
+                    self.devices.CHROMECAST[player.id] = player;
+                    self.emit("devicelist:newdevice", {
+                        type: "ChromeCast",
+                        device: player
+                    });
+                }
+            });
+        }
+    };
 
-	this.findDMRs = function() {
-		if(!this.dlnaSearching) {
-			this.dlnaSearching = true;
-			console.log("Searching for DLNA devices");
-			finder = new RendererFinder();
-			finder.on('found', function(info, msg, desc){
-				console.log("Found a DLNA device: ", msg, info, desc);
-				var client = new MediaRendererClient(msg.location);
-				client.getSupportedProtocols(function(err, protocols) {
-					player = new DMRDevice(client, info, msg, desc, protocols);
-			    	if (!(player.id in self.devices.DLNA)) {
-			    		self.devices.DLNA[player.id] = player;
-				    	self.emit("devicelist:newdevice", {type: "DigitalMediaRenderer", device: player});
-			    	}
-			    });
-			});
-			finder.start(true);
-		}
-	};
+    this.findDMRs = function() {
+        if (!this.dlnaSearching) {
+            this.dlnaSearching = true;
+            console.log("Searching for DLNA devices");
+            finder = new RendererFinder();
+            finder.on('found', function(info, msg, desc) {
+                console.log("Found a DLNA device: ", msg, info, desc);
+                var client = new MediaRendererClient(msg.location);
+                client.getSupportedProtocols(function(err, protocols) {
+                    player = new DMRDevice(client, info, msg, desc, protocols);
+                    if (!(player.id in self.devices.DLNA)) {
+                        self.devices.DLNA[player.id] = player;
+                        self.emit("devicelist:newdevice", {
+                            type: "DigitalMediaRenderer",
+                            device: player
+                        });
+                    }
+                });
+            });
+            finder.start(true);
+        }
+    };
 
-	this.findAppleTVs = function() {
-		if(!this.appleTVSearching) {
-			this.appleTVSearching = true;
+    this.findAppleTVs = function() {
+        if (!this.appleTVSearching) {
+            this.appleTVSearching = true;
 
-			// todo
-		}
+            // todo
+        }
 
-	};
+    };
 
-	this.getDeviceByGUID = function(guid) {
-		var found = null;
-		Object.keys(this.devices).map(function(deviceclass) {
-			this.devices[deviceclass].map(function(device) {
-				if(device.guid == guid) {
-					found = device;
-				}
-			});
-		});
-		return found;
-	};
+    this.getDeviceByGUID = function(guid) {
+        var found = null;
+        Object.keys(this.devices).map(function(deviceclass) {
+            this.devices[deviceclass].map(function(device) {
+                if (device.guid == guid) {
+                    found = device;
+                }
+            });
+        });
+        return found;
+    };
 
 
 }
--- StreamingMediaServer.js
+++ StreamingMediaServer.js
@@ -1,9 +1,10 @@
 var Engine = require('./Engine'),
-	express = require('express'),
-	ip = require('my-local-ip'),
-	findOpenPort = require('./find-open-port'),
-	expressServer = null,
-	app = express();
+    fs = require('fs'),
+    express = require('express'),
+    ip = require('my-local-ip'),
+    findOpenPort = require('./find-open-port'),
+    expressServer = null,
+    app = express();
 
 /**
  * This example starts an express server and serves one url: "/request-from-chromecast"
@@ -12,70 +13,72 @@ var Engine = require('./Engine'),
  */
 
 findOpenPort(3000).then(function(port) {
-	expressServer = app.listen(port, function() {
-	    var port = expressServer.address().port;
+    expressServer = app.listen(port, function() {
+        var port = expressServer.address().port;
 
-	    console.log('streaming-media-encoder Webserver listening at http://%s:%s', ip(), port);
-	});
+        console.log('streaming-media-encoder Webserver listening at http://%s:%s', ip(), port);
+    });
 });
 
-app.get('/debug', function(req,res) {
-	res.header('Content-Type: text/html');
-	res.send(fs.readFileSync(_dirname + '/debug.html'));
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/debug', function(req, res) {
+    res.header('Content-Type: text/html');
+    res.sendFile(__dirname + '/debug.html');
 });
 
-app.get('/debug-info', function(req,res) {
-	res.json(Engine);
+app.get('/debug-info', function(req, res) {
+    res.json(Engine);
 });
 
 app.get('/discover', function(req, res) {
-	console.log("starting discovery");
-	Engine.discover().then(function(devices) {
-		console.log('500ms discovery done', devices);
-		res.json(devices);
-	}).catch(function(E) {
-		throw E;
-	});
+    console.log("starting discovery");
+    Engine.discover().then(function(devices) {
+        console.log('1000ms discovery done', devices);
+        res.json(devices);
+    }).catch(function(E) {
+        throw E;
+    });
 });
 
 app.get('/devicelist', function(req, res) {
-	res.json(Engine.getDeviceList());
+    res.json(Engine.getDeviceList());
 });
 
-app.get('/control/:deviceGUID', function(req,res) {
-	var device = Engine.getDeviceByGUID(req.params.deviceGUID);
-	device.control(req.query.action, req.query).then(res.json);
+app.get('/control/:deviceGUID', function(req, res) {
+    var device = Engine.getDeviceByGUID(req.params.deviceGUID);
+    device.control(req.query.action, req.query).then(res.json);
 });
 
 app.get('/cast/:deviceGUID', function(req, res) {
 
-	var device = Engine.getDeviceByGUID(req.params.deviceGUID);
-	var media;
+    var device = Engine.getDeviceByGUID(req.params.deviceGUID);
+    var media;
 
-	if(req.query.url) {
-		media = new HTTPMedia(req.query.path);
-	}
+    if (req.query.url) {
+        media = new HTTPMedia(req.query.path);
+    }
 
-	if(req.query.path) {
-		media = new LocalFileMedia(req.query.path);
-	}
+    if (req.query.path) {
+        media = new LocalFileMedia(req.query.path);
+    }
 
-	if(req.query.magnet) {
-		media = new TorrentMedia(req.query.magnet);
-	}
-	// cast performs the media analyze
-	// and creating the new streamer for this media on devicename
-	var options = {
-		title: '#ripmatbee',
-		subtitles: './../matbee.srt'
-	};
-	Engine.cast(device, media, options).then(res.json);
+    if (req.query.magnet) {
+        media = new TorrentMedia(req.query.magnet);
+    }
+    // cast performs the media analyze
+    // and creating the new streamer for this media on devicename
+    var options = {
+        title: '#ripmatbee',
+        subtitles: './../matbee.srt'
+    };
+    Engine.cast(device, media, options).then(res.json);
 });
 
 
-app.get('/stream/:streamId', function(req,res) {
-	var streamer = Engine.getStreamer(streamId, app) ;
-	streamer.handle(req,res);
+app.get('/stream/:streamId', function(req, res) {
+    var streamer = Engine.getStreamer(streamId, app);
+    streamer.handle(req, res);
 });
 
 module.exports = app;
