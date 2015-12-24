e82ceffcfba3c48c31b2d31ea84142a287e4a220
SchizoDuckie <schizoduckie@gmail.com>
2015-12-24 23:13:46 +0100

    w.i.p. laying out cast and making a streamer

--- Engine.js
+++ Engine.js
@@ -1,11 +1,12 @@
 var util = require('util'),
-     EventEmitter = require('events'),
-     pump = require('pump'),
-     rangeParser = require('range-parser'),
-     ffmpeg = require('fluent-ffmpeg'),
-     Promise = require('bluebird'),
-     DeviceList = require('./DeviceList'),
-     Streamer = require('./Streamer');
+    EventEmitter = require('events'),
+    pump = require('pump'),
+    rangeParser = require('range-parser'),
+    ffmpeg = require('fluent-ffmpeg'),
+    Promise = require('bluebird'),
+    DeviceList = require('./DeviceList'),
+    Streamer = require('./Streamer'),
+    Analyzer = require('./Analyzer');
 
 /**
  * Converter engine that  knows how to decode video for device profile
@@ -24,23 +25,23 @@ Engine.prototype.discover = function() {
     return DeviceList.discover().then(function(devices) {
         console.log("Fetched device list");
         var d = {};
-                try {
-        Object.keys(devices).map(function(type) {
-            console.log(type);
-            d[type] = {};
-            Object.keys(devices[type]).map(function(guid) {
-                console.log(guid);
-                var dev = devices[type][guid];
-                if(!('toJSON' in dev)) {
-                    debugger;
-                }
-                console.log(devices, type, guid);
+        try {
+            Object.keys(devices).map(function(type) {
+                console.log(type);
+                d[type] = {};
+                Object.keys(devices[type]).map(function(guid) {
+                    console.log(guid);
+                    var dev = devices[type][guid];
+                    if (!('toJSON' in dev)) {
+                        debugger;
+                    }
+                    console.log(devices, type, guid);
                     d[type][guid] = dev.toJSON();
+                });
             });
-        });
-                } catch (E) {
-                    throw E;
-                }
+        } catch (E) {
+            throw E;
+        }
         console.log("device list return: ", d);
         return d;
     });
@@ -54,9 +55,9 @@ Engine.prototype.getDevice = function(DeviceGUID) {
     return DeviceList.getDeviceByGUID(DeviceGUID);
 };
 
-Engine.prototype.createStreamer = function(Media, Device) {
+Engine.prototype.createStreamer = function(Media, Profile, Device) {
     var id = uuid.v4();
-    this.streamers[id]  = new Streamer(id, Media, Device);
+    this.streamers[id] = new Streamer(id, Media, Profile, Device);
     return this.streamers[id];
 };
 
@@ -68,13 +69,19 @@ Engine.prototype.registerStreamer = function(Stream, app) {
     this.streamers[Stream.guid] = Stream;
 };
 
+Engine.prototype.cast = function(Device, Media, options) {
+    var profile = Analyzer.analyze(Media);
+    var streamer = this.createStreamer(Media, Profile, Device);
+    Device.cast(streamer);
+};
+
 util.inherits(Engine, EventEmitter);
 
 Engine.prototype._log = function() {
-  if(this.debug) {
-    console.log("ENGINE: ");
-    console.log.apply(this, arguments);
-  }
+    if (this.debug) {
+        console.log("ENGINE: ");
+        console.log.apply(this, arguments);
+    }
 };
 
 
