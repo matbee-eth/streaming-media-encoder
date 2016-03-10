9be603789fb29aaabaea5495ed9b22fa2d7c72c9
Luigi Poole <luigipoole@outlook.com>
2016-03-09 17:57:12 -0800

    Analyzer

--- Analyzer.js
+++ Analyzer.js
@@ -1,47 +1,37 @@
+import ffmpeg from 'fluent-ffmpeg'
+import ffmpegServer from './FFmpegServer'
+
 // analyzer gets passed a file and knows how to reach the ffmpeg server
 // performs ffmpeg probe on it
 // extracts audio and video streams
-
-var ffmpegServer = require('./FFmpegServer'),
-    ffmpeg = require('fluent-ffmpeg'),
-    Promise = require('bluebird');
-
-
-function Analyzer(Media) {
-    var self = this;
-
-    this.media = Media;
+export default class Analyzer {
+    constructor = Media => this.media = Media;
 
     /**
      * ffprobe gathers information from multimedia streams and prints it in human- and machine-readable fashion.
      * this is needed by the engine to determine format info.
      */
-    this.analyze = function() {
-        console.log("Performing FFMPEG probe");
-        return new Promise(function(resolve, reject) {
-            console.log("Get URL for media: ", self.media, ffmpegServer.getUrl(self.media));
-            ffmpeg.ffprobe(ffmpegServer.getUrl(self.media), function(err, metadata) {
-                console.log("FFProbe returned metadata: ", metadata, err);
-                self.media.setMediaProfile(metadata);
-                resolve(metadata);
-            });
-        });
-    };
+    analyze() {
+        console.log("Performing FFMPEG probe")
+        return new Promise((resolve, reject) => {
+            console.log("Get URL for media: ", this.media, ffmpegServer.getUrl(this.media));
+            ffmpeg.ffprobe(ffmpegServer.getUrl(this.media), (err, metadata) => {
+                console.log("FFProbe returned metadata: ", metadata, err)
+                this.media.setMediaProfile(metadata)
+                resolve(metadata)
+            })
+        })
+    }
 
-
-    this.canPlay = function() {
+    canPlay() {
         if (!this.hasProbed) {
-            return this.probe().then(function(metadata) {
-                return this.canPlay(metadata);
-            }, function(err) {
+            return this.probe().then((metadata) => {
+                return this.canPlay(metadata)
+            }, (err) => {
                 throw new Error("Error during probe on profile!", err);
-            });
+            })
         } else {
-            return this._profile.canPlay(this._probeData);
+            return this._profile.canPlay(this._probeData)
         }
-    };
-
+    }
 }
-
-
-module.exports = Analyzer;
