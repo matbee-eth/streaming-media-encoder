5fcc6c7d7ccc1296222b98688d38a2d2c8cd0518
Luigi Poole <luigipoole@outlook.com>
2016-03-09 17:23:27 -0800

    DeviceList

--- DeviceList.js
+++ DeviceList.js
@@ -1,123 +1,116 @@
-var util = require('util'),
-    EventEmitter = require('events'),
-    // dlna
-    RendererFinder = require('renderer-finder'),
-    MediaRendererClient = require('upnp-mediarenderer-client'),
-    DMRDevice = require('./devices/DigitalMediaRenderer'),
-    // chromecast
-    chromecasts = require('chromecasts'),
-    ChromeCastDevice = require('./devices/ChromeCast'),
+import util from 'util'
+import EventEmitter from 'events'
 
-    HTML5Device = require('./devices/HTML5');
-// appletv
+// dlna
+import RendererFinder from 'renderer-finder'
+import MediaRendererClient from 'upnp-mediarenderer-client'
+import DMRDevice from './devices/DigitalMediaRenderer'
 
-function DeviceList() {
-    EventEmitter.call(this);
-    var self = this;
+// chromecast
+import chromecasts from 'chromecasts'
+import ChromeCastDevice from './devices/ChromeCast'
 
-    this.devices = {
+import HTML5Device from './devices/HTML5'
+
+
+class DeviceList extends EventEmitter {
+
+    devices = {
         HTML5: {},
         CHROMECAST: {},
         DLNA: {},
         APPLETV: {}
     };
 
-    this.chromecastSearching = false;
-    this.dlnaSearching = false;
-    this.appleTVSearching = false;
-    this.HTML5Searching = false;
+    chromecastSearching = false;
+    dlnaSearching = false;
+    appleTVSearching = false;
+    HTML5Searching = false;
 
-    this.discover = function() {
-        console.log("Starting discovery");
-        this.createHTML5Device();
-        this.findChromeCasts();
-        this.findDMRs();
-        this.emit("devicelist:discovering", this);
-        return new Promise(function(resolve) {
-            setTimeout(function() {
-                resolve(self.devices);
-            }, 1000);
-        });
-    };
+    discover() {
+        console.log("Starting discovery")
+        this.createHTML5Device()
+        this.findChromeCasts()
+        this.findDMRs()
+        this.emit("devicelist:discovering", this)
 
-    this.createHTML5Device = function() {
-        if (!this.HTML5Searching) {
-            this.HTML5Searching = true;
-            var player = new HTML5Device();
-            if (!(player.id in self.devices.HTML5)) {
-                self.devices.HTML5[player.id] = player;
+        return new Promise(resolve => setTimeout(() => resolve(self.devices), 1000))
+    }
+
+    createHTML5Device() {
+        if (this.HTML5Searching) return
+
+        this.HTML5Searching = true
+        const player = new HTML5Device()
+
+        if (!(player.id in self.devices.HTML5)) {
+            self.devices.HTML5[player.id] = player
+            self.emit("devicelist:newdevice", {
+                type: "HTML5",
+                device: player
+            })
+        }
+    }
+
+    findChromeCasts() {
+        if (this.chromecastSearching) return
+
+        console.log("Searching for chromecast devices");
+        this.chromecastSearching = true;
+
+        chromecasts().on('update', player => {
+            console.log("Found a chromecast", player.name)
+
+            const newPlayer = new ChromeCastDevice(player)
+            if (!(newPlayer.id in self.devices.CHROMECAST)) {
+                self.devices.CHROMECAST[newPlayer.id] = newPlayer
                 self.emit("devicelist:newdevice", {
-                    type: "HTML5",
-                    device: player
-                });
+                    type: "ChromeCast",
+                    device: newPlayer
+                })
             }
-        }
-    };
+        })
+    }
 
-    this.findChromeCasts = function() {
-        if (!this.chromecastSearching) {
-            console.log("Searching for chromecast devices");
-            this.chromecastSearching = true;
-            chromecasts().on('update', function(player) {
-                console.log("Found a chromecast", player.name);
-                player = new ChromeCastDevice(player);
-                if (!(player.id in self.devices.CHROMECAST)) {
-                    self.devices.CHROMECAST[player.id] = player;
+    findDMRs() {
+        if (this.dlnaSearching) return
+
+        this.dlnaSearching = true;
+        console.log("Searching for DLNA devices");
+
+        finder = new RendererFinder();
+        finder.on('found', (info, msg, desc) => {
+
+            console.log("Found a DLNA device: ", msg, info, desc)
+            const client = new MediaRendererClient(msg.location)
+
+            client.getSupportedProtocols((err, protocols) => {
+                const player = new DMRDevice(client, info, msg, desc, protocols);
+                if (!(player.id in self.devices.DLNA)) {
+                    self.devices.DLNA[player.id] = player;
                     self.emit("devicelist:newdevice", {
-                        type: "ChromeCast",
+                        type: "DigitalMediaRenderer",
                         device: player
-                    });
+                    })
                 }
-            });
-        }
-    };
+            })
+        })
+        finder.start(true)
+    }
 
-    this.findDMRs = function() {
-        if (!this.dlnaSearching) {
-            this.dlnaSearching = true;
-            console.log("Searching for DLNA devices");
-            finder = new RendererFinder();
-            finder.on('found', function(info, msg, desc) {
-                console.log("Found a DLNA device: ", msg, info, desc);
-                var client = new MediaRendererClient(msg.location);
-                client.getSupportedProtocols(function(err, protocols) {
-                    player = new DMRDevice(client, info, msg, desc, protocols);
-                    if (!(player.id in self.devices.DLNA)) {
-                        self.devices.DLNA[player.id] = player;
-                        self.emit("devicelist:newdevice", {
-                            type: "DigitalMediaRenderer",
-                            device: player
-                        });
-                    }
-                });
-            });
-            finder.start(true);
-        }
-    };
+    findAppleTVs() {
+        if (this.appleTVSearching) return
 
-    this.findAppleTVs = function() {
-        if (!this.appleTVSearching) {
-            this.appleTVSearching = true;
+        this.appleTVSearching = true
+    }
 
-            // todo
-        }
-
-    };
-
-    this.getDeviceByGUID = function(guid) {
-        var found = null;
-        Object.keys(this.devices).map(function(deviceclass) {
-            Object.keys(self.devices[deviceclass]).map(function(id) {
-                if (id == guid) {
-                    found = self.devices[deviceclass][guid];
-                }
-            });
-        });
-        return found;
-    };
-
-
+    getDeviceByGUID(guid) {
+        let found = null
+        Object.keys(this.devices).map(deviceclass => Object.keys(self.devices[deviceclass]).map(id => {
+            if (id == guid) found = self.devices[deviceclass][guid]
+        }))
+        return found
+    }
 }
-util.inherits(DeviceList, EventEmitter);
 
-module.exports = new DeviceList();
+export default new DeviceList()
