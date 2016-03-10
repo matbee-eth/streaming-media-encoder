4cb097d481b58c4408a5ee80cfbbbb6cd99a3247
Luigi Poole <luigipoole@outlook.com>
2016-03-09 19:39:14 -0800

    Chromecast

--- src/profiles/Chromecast.js
+++ src/profiles/Chromecast.js
@@ -1,79 +1,69 @@
-var util = require('util'),
-    Promise = require('bluebird');
-    BaseDeviceProfile = require('./BaseDeviceProfile');
+import BaseDeviceProfile from './BaseDeviceProfile'
 
-function ChromeCastProfile() {
-    BaseDeviceProfile.call(this);
 
-    this.audioNeedsTranscodingCodecs = [
+export default class ChromeCastProfile extends BaseDeviceProfile {
+    static audioNeedsTranscodingCodecs = [
         "aac",
         "mp3",
         "vorbis",
     ];
 
-    this.videoNeedsTranscodingCodecs = [
+    static videoNeedsTranscodingCodecs = [
         "h264",
         "x264",
         "vp8"
     ];
 
-    this.validFormats = [
+    static validFormats = [
         "matroska,webm",
         "mov,mp4,m4a,3gp,3g2,mj2",
         "mp3",
         "ogg"
     ];
 
+    getFFmpegFlags(probeData, forceTranscode) {
+        const analysis = this.transcodeNeeded(probeData)
+        const { canPlay, formatNeedsTranscoding, audioNeedsTranscoding, videoNeedsTranscoding, isAudioMedia, isVideoMedia } = analysis
 
-    this.getFFmpegFlags = function (probeData, forceTranscode) {
-        var analysis = this.transcodeNeeded(probeData),
-            canPlay = analysis.needsTranscoding,
-            formatNeedsTranscoding = analysis.formatNeedsTranscoding,
-            audioNeedsTranscoding = analysis.audioNeedsTranscoding,
-            videoNeedsTranscoding = analysis.videoNeedsTranscoding,
-            outputOptions = [],
-            inputOptions = [];
+        let outputOptions = []
+        let inputOptions = []
 
-        if (analysis.isVideoMedia) {
+        if (isVideoMedia) {
             if (audioNeedsTranscoding || audioShiftCorrect) {
-                if(audioShiftCorrect) {
-                    inputOptions.push(audioShiftCorrect);
+                if (audioShiftCorrect) {
+                    inputOptions.push(audioShiftCorrect)
                 }
-                    outputOptions.push("-acodec aac"); // "-acodec libfdk_aac" -> requires custom ffmpeg build from src!
-                } else {
-                    outputOptions.push("-acodec copy");
-                }
-                
-                if (videoNeedsTranscoding || rescaleVideo || subtitle) {
-                    if(subtitle) {
-                        outputOptions.push(subtitle);
-                    }
-                    if(rescaleVideo) {
-                        outputOptions.push(rescaleVideo);
-                    }
-                    outputOptions.push("-vcodec libx264");
-                } else {
-                    outputOptions.push("-vcodec copy");
-                }
-                outputOptions.push("-copyts");
-                outputOptions.push("-preset ultrafast");
-                outputOptions.push("-tune zerolatency");
-                outputOptions.push("-f matroska");
-                
-            } else if (analysis.isAudioMedia) {
-            console.log("VALID AUDIO?", audioNeedsTranscoding);
-            if (audioNeedsTranscoding) {
-                outputOptions.push("-acodec libvorbis");
-                outputOptions.push("-f ogg");
+                outputOptions.push("-acodec aac") // "-acodec libfdk_aac" -> requires custom ffmpeg build from src!
             } else {
-                outputOptions.push("-f " + probeData.format.format_name);
+                outputOptions.push("-acodec copy")
+            }
+
+            if (videoNeedsTranscoding || rescaleVideo || subtitle) {
+                if (subtitle) {
+                    outputOptions.push(subtitle)
+                }
+                if (rescaleVideo) {
+                    outputOptions.push(rescaleVideo)
+                }
+                outputOptions.push("-vcodec libx264")
+            } else {
+                outputOptions.push("-vcodec copy")
+            }
+            outputOptions.push("-copyts")
+            outputOptions.push("-preset ultrafast")
+            outputOptions.push("-tune zerolatency")
+            outputOptions.push("-f matroska")
+
+        } else if (isAudioMedia) {
+            console.log("VALID AUDIO?", audioNeedsTranscoding)
+            if (audioNeedsTranscoding) {
+                outputOptions.push("-acodec libvorbis")
+                outputOptions.push("-f ogg")
+            } else {
+                outputOptions.push("-f " + probeData.format.format_name)
             }
         }
-        return new Promise(function(resolve) {
-            resolve(inputOptions, outputOptions);
-        });
-    };
-}
-util.inhertis(ChromeCastProfile, BaseDeviceProfile);
 
-module.exports = new ChromeCastProfile();
+        return new Promise(resolve => resolve(inputOptions, outputOptions))
+    }
+}
