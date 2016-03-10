31a8f5ca3936a6724a828fe53aaf2e1890068087
Luigi Poole <luigipoole@outlook.com>
2016-03-09 17:49:30 -0800

    FFMpegServer

--- FFmpegServer.js
+++ FFmpegServer.js
@@ -1,62 +1,57 @@
-var findOpenPort = require('./find-open-port'),
-    ffmpeg = require('fluent-ffmpeg'),
-    express = require('express');
+import express from 'express'
+import ffmpeg from 'fluent-ffmpeg'
+import findOpenPort from './find-open-port'
+import Engine from './Engine'
 
 
-FFMpegServer = function() {
+class FFMpegServer {
+    constructor() {
+        this.app = express()
+        this.server = null
+        this.port = 3001
 
-    var self = this;
-    this.port = 3001;
-    this.server = null;
-    this.app = express();
 
-    findOpenPort(3001).then(function(port) {
-        self.port = port;
-        self.server = self.app.listen(port, function() {
-            console.log('FFmpeg Webserver listening at %s', self.getUrl());
-        });
-    });
+        findOpenPort(3001).then(port => {
+            this.port = port
+            this.server = this.app.listen(port, () console.log('FFmpeg Webserver listening at %s', this.getUrl()))
+        })
 
-    /**
-     * ffmpeg passthrough for GET requests to fetch a file
-     * @param  {Request} req httprequest
-     * @param  {Response} res http response
-     */
-    this.app.get('/:fileId', function(req, res) {
-        console.log('ENGINE!', require('./Engine'));
-        var streamer = require('./Engine').getStreamer(req.params.fileId);
-        if (streamer) {
-            streamer.handle('GET', req, res);
-        } else {
-            res.end();
-        }
-    });
 
-    /**
-     * ffmpeg passthrough for HEAD requests to fetch a file
-     * @param  {Request} req httprequest
-     * @param  {Response} res http response
-     */
-    this.app.head('/:fileId', function(req, res) {
-        var streamer = require('./Engine').getStreamer(req.params.fileId);
-        if (streamer) {
-            streamer.handle('HEAD', req, res);
-        } else {
-            res.end();
-        }
-    });
+        /**
+         * ffmpeg passthrough for GET requests to fetch a file
+         * @param  {Request} req httprequest
+         * @param  {Response} res http response
+         */
+        this.app.get('/:fileId', (req, res) => {
+            console.log('ENGINE!', Engine)
+            const streamer = Engine.getStreamer(req.params.fileId)
+            if (streamer)
+                streamer.handle('GET', req, res)
+            else
+                res.end()
+        })
+
+
+        /**
+         * ffmpeg passthrough for HEAD requests to fetch a file
+         * @param  {Request} req httprequest
+         * @param  {Response} res http response
+         */
+        this.app.head('/:fileId', (req, res) => {
+            const streamer = Engine.getStreamer(req.params.fileId)
+            if (streamer)
+                streamer.handle('HEAD', req, res)
+            else
+                res.end()
+        })
+    }
 
     /**
      * Fetch loopback url for file or return base url
      * @param  {string} fileId Optional fileId to append
      * @return {string} url
      */
-    this.getUrl = function(Media) {
-        return "http://127.0.0.1:" + this.port + "/" + (Media ? Media.id : '');
-    };
+    getUrl = Media => `http://127.0.0.1:${this.port}/${(Media ? Media.id : '')}`
+}
 
-
-};
-
-
-module.exports = new FFMpegServer();
+export default new FFMpegServer()
