83148bbc26f895fbcc3600c4598af912a9dfe379
Luigi Poole <luigipoole@outlook.com>
2016-03-09 18:53:21 -0800

    BaseMedia

--- src/media/BaseMedia.js
+++ src/media/BaseMedia.js
@@ -1,46 +1,24 @@
-var uuid = require('node-uuid');
+import { v4 as uuid } from 'node-uuid'
+
 
 // BaseMedia is a generic wrapper for file, url, magnet, torrent, stream
+export default class BaseMedia {
+    static id = uuid();
+    isAnalyzed = false;
+    profile = null;
+    filesize = null;
+    filename = null;
 
-function BaseMedia() {
-    this.id = uuid.v4();
-    this.isAnalyzed = false;
-    this.profile = null;
-    this.filesize = null;
-    this.filename = null; // 'RIPmatbee.mp4'
+    setSize = size => this.filesize = size;
+    setFileName = size => this.filesize = size;
+
+    setMediaProfile(profile) {
+        console.log("Base Media Profile set for ", this.filename, profile)
+        this.profile = profile
+        this.isAnalyzed = true
+    }
+
+    streamNeeded(startByte, endByte, cb) {
+        throw new Error("StreamNeeded: Not implemented!", this)
+    }
 }
-
-BaseMedia.prototype.getSize = function() {
-    return this.filesize;
-};
-
-BaseMedia.prototype.setSize = function(size) {
-    this.filesize = size;
-    return this;
-};
-
-BaseMedia.prototype.setFileName = function(name) {
-    this.filename = name;
-    return this;
-};
-
-BaseMedia.prototype.getFileName = function() {
-    return this.name;
-};
-
-BaseMedia.prototype.getMediaProfile = function() {
-    return this.profile;
-};
-
-BaseMedia.prototype.setMediaProfile = function(profile) {
-    console.log("Base Media Profile set for ", this.filename, profile);
-    this.profile = profile;
-    this.isAnalyzed = true;
-    return this;
-};
-
-BaseMedia.prototype.streamNeeded = function(startByte, endByte, cb) {
-    throw new Error("StreamNeeded: Not implemented!", this);
-};
-
-module.exports = BaseMedia;
