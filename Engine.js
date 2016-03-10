59af01b9b67cdba283037fad2a2078fe590a8081
Luigi Poole <luigipoole@outlook.com>
2016-03-09 17:41:32 -0800

    Engine

--- Engine.js
+++ Engine.js
@@ -1,12 +1,13 @@
-var util = require('util'),
-    EventEmitter = require('events'),
-    uuid = require('node-uuid'),
-    pump = require('pump'),
-    rangeParser = require('range-parser'),
-    Promise = require('bluebird'),
-    DeviceList = require('./DeviceList'),
-    Streamer = require('./Streamer'),
-    Analyzer = require('./Analyzer');
+import util from 'util'
+import EventEmitter from 'events'
+import { v4 as uuid } from 'node-uuid'
+import pump from 'pump'
+import rangeParser from 'range-parser'
+
+import DeviceList from './DeviceList'
+import Streamer from './Streamer'
+import Analyzer from './Analyzer'
+
 
 /**
  * Converter engine that  knows how to decode video for device profile
@@ -15,79 +16,56 @@ var util = require('util'),
  * @param {uuid} id UUID for this engine instance
  * @param {string} url url where media can be found on the ffmpeg server
  */
-function Engine() {
-    EventEmitter.call(this);
-    this.debug = true;
-    this.streamers = {};
+class Engine extends EventEmitter {
+    streamers = {};
 
-    var self = this;
-
-    this.discover = function() {
-        return DeviceList.discover().then(function(devices) {
-            var d = {};
-            try {
-                Object.keys(devices).map(function(type) {
-                    console.log(type);
-                    d[type] = {};
-                    Object.keys(devices[type]).map(function(guid) {
-                        var dev = devices[type][guid];
-                        d[type][guid] = dev.toJSON();
-                    });
-                });
-            } catch (E) {
-                throw E;
-            }
-            return d;
-        });
-    };
-
-    this.getDeviceList = function() {
-        return DeviceList;
-    };
-
-    this.getDevice = function(DeviceGUID) {
-        return DeviceList.getDeviceByGUID(DeviceGUID);
-    };
-
-    this.createStreamer = function(Media, Device) {
-        var id = uuid.v4();
-        this.streamers[id] = new Streamer(id, Media, Device);
-        return this.streamers[id];
-    };
-
-    this.getStreamer = function(StreamGUID) {
-        return this.streamers[StreamGUID];
-    };
-
-    this.registerStreamer = function(Stream) {
-        this.streamers[Stream.guid] = Stream;
-    };
-
-    this.cast = function(Device, Media, options) {
-        var analyzer = new Analyzer(Media);
-        return analyzer.analyze().then(function(Profile) {
-            Media.setMediaProfile(Profile);
-            var streamer = self.createStreamer(Media, Device);
-            return Device.cast(streamer.getUrl(), options);
-        });
-    };
-
-
-    this._log = function() {
-        if (this.debug) {
-            console.log("ENGINE: ");
-            console.log.apply(this, arguments);
+    discover = () => DeviceList.discover().then(devices => {
+        const d = {}
+        try {
+            Object.keys(devices).map(type => {
+                console.log(type)
+                d[type] = {}
+                Object.keys(devices[type]).map((guid) => {
+                    var dev = devices[type][guid]
+                    d[type][guid] = dev.toJSON()
+                })
+            })
+        } catch (E) {
+            throw E
         }
-    };
+        return d
+    });
+
+    getDeviceList = () => DeviceList;
+
+    getDevice = DeviceGUID => DeviceList.getDeviceByGUID(DeviceGUID);
+
+    createStreamer(Media, Device) {
+        const id = uuid();
+        this.streamers[id] = new Streamer(id, Media, Device)
+        return this.streamers[id]
+    }
+
+    getStreamer = StreamGUID => this.streamers[StreamGUID];
+
+    registerStreamer = Stream => this.streamers[Stream.guid] = Stream;
+
+    cast(Device, Media, options) {
+        const analyzer = new Analyzer(Media)
+        return analyzer.analyze().then((Profile) => {
+            Media.setMediaProfile(Profile);
+            const streamer = this.createStreamer(Media, Device)
+            return Device.cast(streamer.getUrl(), options)
+        })
+    }
+
+    _log() {
+        if (!this.debug) return
+
+        console.log("ENGINE: ")
+        console.log.apply(this, arguments)
+    }
 }
 
 
-
-util.inherits(Engine, EventEmitter);
-
-
-console.log("CREATING NEW ENGINE!");
-var engine = new Engine();
-
-
-module.exports = engine;
+export default new Engine()
